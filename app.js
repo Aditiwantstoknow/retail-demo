f17b9a4450a3e5fd8c66f8f811dd6d785b688ab1
Aditi Joshi <146760949+Aditiwantstoknow@users.noreply.github.com>
2026-04-09 01:52:07 +0530

    update in app.js for testing

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 async function loadData() {
-    const res = await fetch("YOUR_API_GATEWAY_URL");
+    const res = await fetch("https://bb0kg06t4g.execute-api.us-east-1.amazonaws.com/content");
     const data = await res.json();
 
     document.getElementById("output").innerText = data.message;
